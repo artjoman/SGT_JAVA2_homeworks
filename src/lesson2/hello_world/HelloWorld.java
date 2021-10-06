d7cc84941db917ff7da25d36cbaf42edac069564
shegoestech202122 <artjoms.v.petrovs@gmail.com>
2021-10-06 18:54:34 +0300

    Lesson 2 - Task added

--- src/lesson2/hello_world/HelloWorld.java
+++ src/lesson2/hello_world/HelloWorld.java
@@ -2,10 +2,16 @@ package lesson2.hello_world;
 
 public class HelloWorld {
 
-    public String helloWorld() {
+    public static String helloWorld() {
         String hello = "";
 
+        // Enter code here
 
         return hello;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(helloWorld());
+
     }
 }
