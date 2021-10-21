5a4717fddd7a5c7bbe7e831a10b5a9ef045116b1
Laila Duffy <lail.duffy@gmail.com>
2021-10-21 14:19:01 +0100

    Revert "Lesson 2 - Task added"
    This reverts commit d7cc84941db917ff7da25d36cbaf42edac069564.
    
    # Conflicts:
    #	src/lesson2/hello_world/HelloWorld.java

--- src/lesson2/hello_world/HelloWorld.java
+++ src/lesson2/hello_world/HelloWorld.java
@@ -2,16 +2,10 @@ package lesson2.hello_world;
 
 public class HelloWorld {
 
-    public static String helloWorld() {
+    public String helloWorld() {
         String hello = "";
 
-        // Enter code here
 
         return hello;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(helloWorld());
-
     }
 }
