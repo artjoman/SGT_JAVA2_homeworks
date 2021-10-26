5631be262bdc189428773ecb5c7f8694ef91bf15
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-26 20:51:03 +0300

    Task 1. calcDistance(otherPlanet) method added

--- src/lv/bootcamp/inheritance/task1/Planet.java
+++ src/lv/bootcamp/inheritance/task1/Planet.java
@@ -7,16 +7,37 @@ public class Planet extends SpaceObject {
     private double velocityY;
 
     //Constructors
+
+
     public Planet() {
     }
 
-    public Planet(double mass, double velocityX, double velocityY) {
+    public Planet(String name, String type, double width, double height, String color, double coordinateX, double coordinateY, double mass, double velocityX, double velocityY) {
+        super(name, type, width, height, color, coordinateX, coordinateY);
         this.mass = mass;
         this.velocityX = velocityX;
         this.velocityY = velocityY;
     }
 
     //Methods
+
+    /**
+     * Method - calcDistance(otherPlanet).
+     * This method calculates the distance between two Planets.
+     * Realized formula --> r2 = dx2 + dy2
+     * @param planet
+     * @return
+     */
+    public double calcDistance(Planet planet) {
+        double x1 = getCoordinateX();
+        double y1 = getCoordinateY();
+        double x2 = planet.getCoordinateX();
+        double y2 = planet.getCoordinateY();
+        double distanceBetweenPlanets;
+        distanceBetweenPlanets=Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+        System.out.println("Distance between " + "("+x1+","+y1+" ) & "+"("+x2+","+y2+") ===> "+distanceBetweenPlanets);
+        return distanceBetweenPlanets;
+    }
 
 
     //Getters and Setters
