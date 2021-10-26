ba9cdfc3e579f8ad6e63f62f994c21c6ea16f44c
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-26 19:42:29 +0300

    Task 1. Getters&Setters + Constructors

--- src/lv/bootcamp/inheritance/task1/Planet.java
+++ src/lv/bootcamp/inheritance/task1/Planet.java
@@ -5,4 +5,39 @@ public class Planet extends SpaceObject {
     private double mass;
     private double velocityX;
     private double velocityY;
+
+    //Constructors
+    public Planet() {
+    }
+
+    public Planet(double mass, double velocityX, double velocityY) {
+        this.mass = mass;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+    }
+
+    //Getters and Setters
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getVelocityX() {
+        return velocityX;
+    }
+
+    public void setVelocityX(double velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public double getVelocityY() {
+        return velocityY;
+    }
+
+    public void setVelocityY(double velocityY) {
+        this.velocityY = velocityY;
+    }
 }
