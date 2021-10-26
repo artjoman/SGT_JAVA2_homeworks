b6f420d4d000fb5c2c5aadd5c85c6d222987783a
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-26 19:38:51 +0300

    Task 1. Attributes

---
+++ src/lv/bootcamp/inheritance/task1/Planet.java
@@ -0,0 +1,8 @@
+package lv.bootcamp.inheritance.task1;
+
+public class Planet extends SpaceObject {
+
+    private double mass;
+    private double velocityX;
+    private double velocityY;
+}
--- src/lv/bootcamp/inheritance/task1/SpaceObject.java
+++ src/lv/bootcamp/inheritance/task1/SpaceObject.java
@@ -1,4 +1,14 @@
 package lv.bootcamp.inheritance.task1;
 
 public class SpaceObject {
+
+    private String name;
+    private String type;
+    private double width;
+    private double height;
+    private String color;
+    private double coordinateX;
+    private double coordinateY;
+
+
 }
