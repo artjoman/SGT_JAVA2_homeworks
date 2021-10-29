389c7c09df14275cdadb41a3d683836d7e4181a3
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-29 18:57:50 +0300

    Task 2. Getters and Setters.

--- src/lv/bootcamp/inheritance/task2/Spaceship.java
+++ src/lv/bootcamp/inheritance/task2/Spaceship.java
@@ -12,5 +12,60 @@ public class Spaceship {
     private String fuel;
     private double fuelConsumption;
 
+    //Getters and Setters
+    public double getMass() {
+        return mass;
+    }
 
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getVelocityX() {
+        return velocityX;
+    }
+
+    public void setVelocityX(double velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public double getVelocityY() {
+        return velocityY;
+    }
+
+    public void setVelocityY(double velocityY) {
+        this.velocityY = velocityY;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
+    public double getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(double acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
 }
