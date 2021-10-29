97d54edf6aeb277dae0580008cb1fc8f08459774
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-29 18:56:23 +0300

    Task 2. Attributes for Spaceship Class

---
+++ src/lv/bootcamp/inheritance/task2/Spaceship.java
@@ -0,0 +1,16 @@
+package lv.bootcamp.inheritance.task2;
+
+import lv.bootcamp.inheritance.task1.SpaceObject.*;
+
+public class Spaceship {
+
+    private double mass;
+    private double velocityX;
+    private double velocityY;
+    private double direction;
+    private double acceleration;
+    private String fuel;
+    private double fuelConsumption;
+
+
+}
